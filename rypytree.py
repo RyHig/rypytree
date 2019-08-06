a9e99b7b42ca34d8de2c3607c4238fbb35a5e528
Ryan Higgins <ryanjeffhiggins@gmail.com>
2019-08-06 11:05:35 -0500

    kind of broken right now... added a pass for last_line

--- rypytree.py
+++ rypytree.py
@@ -1,7 +1,7 @@
 import pathlib
 import argparse
-import logging
 
+LINES = '│  '
 
 def create_parser():
 
@@ -12,37 +12,49 @@ def create_parser():
         prog='PROG')
     parser.add_argument('directory', metavar='/path/to/dir')
     parser.add_argument('-a', action='store_true')
-    parser.add_argument('-f', action='store_true')
+    parser.add_argument('-d')
     return parser
 
 
 '''
 TODO: 
-Save the data into a Dictionary of Dictionaries.
-Maybe add depth?
+Save the data into a Dictionary of Dictionaries?
 Figure out how to add the tree-like display with the pipes.
-
 '''
-def how_far(dir, hidden):
-    if dir.is_dir():
-        items_in_folder = [item for item in dir.iterdir()]
-        for item in items_in_folder:
-            if (item.name).startswith('.') and not hidden:
-                pass
-            elif item.is_dir():
-                how_far(item,hidden)
-                print('Dir: {}'.format(item))
-            else:
-                logging.debug('This file is not a directory.')
-                print('File: {}'.format(item))
+def how_far(directory, hidden, limit, n=0, last_line=False):
+    if n == 0:
+        print(directory)
+
+    if n == limit:
+        return
+    elif directory.is_dir():
+        items_in_folder = [item for item in directory.iterdir()]
+        
+        if last_line == True:
+            pass
+        else:
+            for item in range(len(items_in_folder)):
+                if (items_in_folder[item].name).startswith('.') and not hidden:
+                    pass
+                elif item == len(items_in_folder) - 1:
+                    if items_in_folder[item].is_dir():
+                        print('{}{} DIR:{}'.format(LINES*n, "└─", items_in_folder[item].name))
+                        how_far(items_in_folder[item], hidden, limit, n+1, last_line=True)
+                    
+                    else:
+                        print('{}{} FILE:{}'.format(LINES*n, "└─", items_in_folder[item].name))
+                elif items_in_folder[item].is_dir():
+                    print('{}{} DIR:{}'.format(LINES*n, "├─", items_in_folder[item].name))
+                    how_far(items_in_folder[item], hidden, limit, n+1)
+                    
+                else:
+                    print('{}{} FILE:{}'.format(LINES*n, "├─", items_in_folder[item].name))
     else:
-        logging.debug('This file is not a directory.')
         return None
 
 
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
-    print(args)
     p = pathlib.Path(args.directory)
-    how_far(p, args.a)
+    how_far(p, args.a,int(args.d))
